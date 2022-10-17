945e683afc5f375b0ba79562ec7f61c375d0786c
JuanAV21 <jfarizpevega20@students.desu.edu>
2022-10-16 23:30:47 -0400

    OpenFile_explode

--- lab1.c
+++ lab1.c
@@ -5,12 +5,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* readString(char* fileName){
-    FILE *fopen(const char* filename, const char)
+    char * str;
+    FILE *fopen(const char* fileName, "r");
+    fgets(str, 100, (FILE*)fopen);
+    return str;
 }
 
 char* mysteryExplode(const char* str){
+    int * len = strlen(str);
+    int * cnt = 0;
+    char * cout;
 
+    for(int i =0;i =<len;++i){
+        cnt += i;
+    }
+
+    cout = malloc(cnt * sizeof(char))
+
+    for(int i=0;i<len;i++){
+
+        for(int y=0;y<i;y++){
+            cout += str.charat(y);
+        }
+
+    }
+
+    return cout;
 }
 
