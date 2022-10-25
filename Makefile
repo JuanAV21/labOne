e366728b54c3b924be821a184c22925670462a43
JuanAV21 <jfarizpevega20@students.desu.edu>
2022-10-25 09:36:36 -0400

    progress

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 hellomake: main.c lab1.c
-	gcc -o explode main.c lab1.c
+	gcc -o lab1 main.c lab1.c
 
 clean:
-	%(RM) explode
+	%(RM) labe1
--- lab1.c
+++ lab1.c
@@ -6,35 +6,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lab1.h"
 
 char* readString(char* fileName){
-    char * str = malloc(326 * sizeof(char));
+    char * str = malloc(MAX_LINE_LEN * sizeof(char));
     FILE *fptr = fopen(fileName, "r");
-    fgets(str, 26, (FILE*)fptr);
+    fgets(str, MAX_LINE_LEN, (FILE*)fptr);
     fclose(fptr);
-    free(fptr);
+    printf("1: %s", str);
     return str;
 }
 
-char* mysteryExplode(const char* str){
-    int len = strlen(str);
+char* mysteryExplode(const char * str){
+    int len = strlen(str) - 1;
     int cnt = 0;
     char * cout;
+    printf("3: %s\n", str);
+    printf("len %d\n", len);
 
-    for(int i =0;i <= len;++i){
-        cnt += i;
+    for(int i =0; i <= len ; ++i){
+        cnt = cnt + i;
     }
 
-    cout = malloc(cnt * sizeof(char));
-
+    printf("Expected value 10\n");
+    printf("Actual value: %d\n", cnt);
+    printf("sizeof: %ld\n", sizeof(char));
+    cout = malloc(cnt * sizeof(char) + 1);
+    cnt = 0;
     for(int i=0;i<len;i++){
 
-        for(int y=0;y<i;y++){
+        for(int y=0;y<=i;y++){
             //cout += str.charat(y);
-            cout += str[y];
+            cout[cnt] = str[y];
+            printf("y: %d\n", y);
+            cnt += 1;
+            //printf("cout: %s\n", &str[y]);
+
         }
 
     }
+    printf("cout: %s\n", cout);
+    printf("str: %s", str);
     return cout;
 }
 
