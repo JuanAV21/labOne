5484fd8946e278e5779480759b5824b166f6f8fb
JuanAV21 <jfarizpevega20@students.desu.edu>
2022-10-25 12:04:05 -0400

    Finished

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 hellomake: main.c lab1.c
-	gcc -o lab1 main.c lab1.c
+	gcc -o _explode_ main.c lab1.c
 
 clean:
 	%(RM) labe1
--- lab1.c
+++ lab1.c
@@ -13,7 +13,6 @@ char* readString(char* fileName){
     FILE *fptr = fopen(fileName, "r");
     fgets(str, MAX_LINE_LEN, (FILE*)fptr);
     fclose(fptr);
-    printf("1: %s", str);
     return str;
 }
 
@@ -21,32 +20,22 @@ char* mysteryExplode(const char * str){
     int len = strlen(str) - 1;
     int cnt = 0;
     char * cout;
-    printf("3: %s\n", str);
-    printf("len %d\n", len);
 
     for(int i =0; i <= len ; ++i){
         cnt = cnt + i;
     }
 
-    printf("Expected value 10\n");
-    printf("Actual value: %d\n", cnt);
-    printf("sizeof: %ld\n", sizeof(char));
     cout = malloc(cnt * sizeof(char) + 1);
     cnt = 0;
     for(int i=0;i<len;i++){
 
         for(int y=0;y<=i;y++){
-            //cout += str.charat(y);
             cout[cnt] = str[y];
-            printf("y: %d\n", y);
             cnt += 1;
-            //printf("cout: %s\n", &str[y]);
 
         }
 
     }
-    printf("cout: %s\n", cout);
-    printf("str: %s", str);
     return cout;
 }
 
